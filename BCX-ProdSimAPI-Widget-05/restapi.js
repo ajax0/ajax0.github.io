0f2446128f2694521d04290c0e8ceedcf2defac0
ajax0 <andrew@jabbitt.com>
2022-06-14 11:32:06 +0200

    ferrygun3

--- BCX-ProdSimAPI-Widget-05/restapi.js
+++ BCX-ProdSimAPI-Widget-05/restapi.js
@@ -206,7 +206,7 @@
             console.log("--First Time --");
 
             let div0 = document.createElement('div');
-            div0.innerHTML = '<?xml version="1.0"?><script id="oView_' + widgetName + '" name="oView_' + widgetName + '" type="sapui5/xmlview"><mvc:View xmlns="sap.m" xmlns:mvc="sap.ui.core.mvc" xmlns:core="sap.ui.core" xmlns:l="sap.ui.layout" height="100%" controllerName="myView.Template"><l:VerticalLayout class="sapUiContentPadding" width="100%"><l:content><Input id="input"  placeholder="Enter partner number..." liveChange=""/></l:content><Button id="buttonId" class="sapUiSmallMarginBottom" text="Get Score" width="150px" press=".onButtonPress" /></l:VerticalLayout></mvc:View></script>';
+            div0.innerHTML = '<?xml version="1.0"?><script id="oView_' + widgetName + '" name="oView_' + widgetName + '" type="sapui5/xmlview"><mvc:View xmlns="sap.m" xmlns:mvc="sap.ui.core.mvc" xmlns:core="sap.ui.core" xmlns:l="sap.ui.layout" height="100%" controllerName="myView.Template"><l:VerticalLayout class="sapUiContentPadding" width="100%"><l:content><Input id="input"  placeholder="Enter partner number..." liveChange=""/></l:content><Button id="buttonId" class="sapUiSmallMarginBottom" text="Get Similar Products" width="150px" press=".onButtonPress" /></l:VerticalLayout></mvc:View></script>';
             _shadowRoot.appendChild(div0);
 
             let div1 = document.createElement('div');
@@ -252,7 +252,10 @@
                             contentType: 'application/x-www-form-urlencoded',
                             success: function(data) {
                                 console.log(data);
-                                _score = data;
+                                let csvString = Object.keys(data.similar_products).map(field => data.similar_products[field]).join();
+                                console.log(csvString);
+
+                                _score = csvString;
 
                                 that._firePropertiesChanged();
                                 this.settings = {};

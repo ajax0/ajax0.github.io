045480d7aa0ed2671baf4beb2287963e3fab3dc4
ajax0 <andrew@jabbitt.com>
2022-06-20 15:06:21 +0200

    Update restapi.js

--- BCX-ProdSimAPI-Widget-05/restapi.js
+++ BCX-ProdSimAPI-Widget-05/restapi.js
@@ -280,9 +280,9 @@
                                 _score = data["topn"];
                                 _topn =  data["topn"];
                                 let concatcoords = '';
-                                _coords = concatcoords.concat(data["x_coords"], "," , data["y_coords"], "," , data["z_coords"]);
+                                _coords = concatcoords.concat(data["x_coords"], ";" , data["y_coords"], ";" , data["z_coords"]);
                                 console.log(_coords);
-                                const datasetarray = _coords.split(',');
+                                const datasetarray = _coords.split(';');
                                 console.log("x_set = ", datasetarray[0]);
                                 that._firePropertiesChanged();
                                 this.settings = {};

a813b430fa767681f9be24fe8a7085d81b0c478f
ajax0 <andrew@jabbitt.com>
2022-06-20 19:47:36 +0200

    Update scatterplot.js

--- scatterplot/scatterplot.js
+++ scatterplot/scatterplot.js
@@ -106,8 +106,8 @@ e.exports=function(t){return null!=t&&(n(t)||function(t){return"function"==typeo
 
 			var myProps = this._props
 			console.log("this._props = ", this._props);
-			console.log(myProps(["value"]));
-			const datasetarray = myProps(["value"]).split(';');
+			console.log(myProps["value"]);
+			const datasetarray = myProps["value"].split(';');
 			console.log("datasetarray[0]:", datasetarray[0]);
 
 			var x_data = datasetarray[0];

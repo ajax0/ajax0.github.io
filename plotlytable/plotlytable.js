2563edf645475907cda2f0692fe32d282d63acf0
ajax0 <andrew@jabbitt.com>
2022-07-08 11:25:13 +0200

    Update plotlytable.js

--- plotlytable/plotlytable.js
+++ plotlytable/plotlytable.js
@@ -396,7 +396,7 @@ e.exports=function(t){return null!=t&&(n(t)||function(t){return"function"==typeo
 				var arrayWidth = labelarray[0].length;
 				for (const row of labelarray){
 					console.log("row:", row);
-					pct = parseFloat(row[2]) * 100;
+					pct = parseFloat(row(2)) * 100;
 					console.log("pct:", pct);
 				}
 				console.log("labelarray", labelarray);
